fe714a3781a383b902a966fa7abe43060ff49be0
quanptit <quanptit410@gmail.com>
2024-12-27 19:58:22 +0700

    ok

--- lib/rating_dialog/dialog_rating.dart
+++ lib/rating_dialog/dialog_rating.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:q_common_utils/edge_s.dart';
 import 'package:q_common_utils/l.dart';
-import 'package:q_common_utils/languages/laguage_utils.dart';
+import 'package:q_common_utils/laguage_utils.dart';
 import 'package:q_common_utils/open_url_utils.dart';
 import 'package:q_common_utils/route_utils.dart';
 import 'package:q_common_utils/ui_utils.dart';
