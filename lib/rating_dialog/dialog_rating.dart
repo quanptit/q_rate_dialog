ab93c825fec3df15debb688cf95aa76e1e2d4a72
quanptit <quanptit410@gmail.com>
2024-12-31 15:16:10 +0700

    update

--- lib/rating_dialog/dialog_rating.dart
+++ lib/rating_dialog/dialog_rating.dart
@@ -9,11 +9,12 @@ import 'package:q_common_utils/laguage_utils.dart';
 import 'package:q_common_utils/open_url_utils.dart';
 import 'package:q_common_utils/route_utils.dart';
 import 'package:q_common_utils/ui_utils.dart';
-import 'package:q_theme/app_theme.dart';
+import 'package:q_theme/q_theme.dart';
 
 import '../app_rate_utils.dart';
 
-void showDialogRating(BuildContext context, {required String linkContract, String? appStoreId}) async {
+void showDialogRating(BuildContext context,
+    {required String linkContract, String? appStoreId}) async {
   // if (await AppRateUtils.needShowRateSuggess()) {
   showGeneralDialog(
     context: context,
@@ -44,7 +45,8 @@ class _DialogRatingState extends State<DialogRating> {
   void btnSubmitClick() {
     if (ratingValue == 0) {
       L.d("ratingValue 0");
-      UiUtils.showSnackBar(LanguagesUtils.getString("not_given_star", "You have not given stars!"));
+      UiUtils.showSnackBar(LanguagesUtils.getString(
+          "not_given_star", "You have not given stars!"));
       return;
     }
     if (ratingValue > 4.5) {
@@ -67,12 +69,13 @@ class _DialogRatingState extends State<DialogRating> {
   @override
   Widget build(BuildContext context) {
     Widget child;
-    if (_rateState == _RateState.good)
+    if (_rateState == _RateState.good) {
       child = _buildRateStateGood(context);
-    else if (_rateState == _RateState.notGood)
+    } else if (_rateState == _RateState.notGood) {
       child = _buildRateStateNotGood(context);
-    else
+    } else {
       child = _buildRateStateNone(context);
+    }
 
     bool isSmall = CommonUtils.isSmall(context);
 
@@ -81,9 +84,11 @@ class _DialogRatingState extends State<DialogRating> {
       dismisAble: true,
       child: Center(
         child: Card(
-          margin: EdgeInsets.symmetric(horizontal: isSmall ? 16 : 32, vertical: isSmall ? 16 : 32),
+          margin: EdgeInsets.symmetric(
+              horizontal: isSmall ? 16 : 32, vertical: isSmall ? 16 : 32),
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: isSmall ? 12 : 16, vertical: 20),
+            padding: EdgeInsets.symmetric(
+                horizontal: isSmall ? 12 : 16, vertical: 20),
             child: child,
           ),
         ),
@@ -92,49 +97,49 @@ class _DialogRatingState extends State<DialogRating> {
   }
 
   Widget _buildRateStateGood(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            LanguagesUtils.getString("thank_you", "Thank you!"),
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          E.sizeBoxHeight_16,
-          Text(
-              LanguagesUtils.getString(
-                  "rate_msg2", "Please help us out - It'd be really helpful if you rate us on store."),
-              style: const TextStyle(fontSize: 16),
-              textAlign: TextAlign.center),
-          E.sizeBoxHeight_32,
-          Wrap(
-            direction: Axis.horizontal,
-            alignment: WrapAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: btnNotNowClick,
-                child: Text(
-                  LanguagesUtils.getString("Notnow", "Not now").toUpperCase(),
-                  style: TextStyle(fontSize: 16, color: Colors.black45),
-                ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          LanguagesUtils.getString("thank_you", "Thank you!"),
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        E.sizeBoxHeight_16,
+        Text(
+            LanguagesUtils.getString("rate_msg2",
+                "Please help us out - It'd be really helpful if you rate us on store."),
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center),
+        E.sizeBoxHeight_32,
+        Wrap(
+          direction: Axis.horizontal,
+          alignment: WrapAlignment.spaceAround,
+          children: [
+            TextButton(
+              onPressed: btnNotNowClick,
+              child: Text(
+                LanguagesUtils.getString("Notnow", "Not now").toUpperCase(),
+                style: TextStyle(fontSize: 16, color: T.getColors(context).secondary),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  RouteUtils.backPress(context: context);
-                  AppRateUtils.openStoreForReview(appStoreId: widget.appStoreId);
-                  AppRateUtils.setUserHasGoStoreRateApp();
-                },
-                child: Text(
-                  LanguagesUtils.getString("yes_sure", "Yes, sure!").toUpperCase(),
-                  style: TextStyle(fontSize: 16),
-                ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                RouteUtils.backPress(context: context);
+                AppRateUtils.openStoreForReview(
+                    appStoreId: widget.appStoreId);
+                AppRateUtils.setUserHasGoStoreRateApp();
+              },
+              child: Text(
+                LanguagesUtils.getString("yes_sure", "Yes, sure!")
+                    .toUpperCase(),
+                style: TextStyle(fontSize: 16),
               ),
-            ],
-          )
-        ],
-      ),
+            ),
+          ],
+        )
+      ],
     );
   }
 
@@ -144,29 +149,39 @@ class _DialogRatingState extends State<DialogRating> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text(LanguagesUtils.getString("How_make_better", "How can we make it better?"),
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text(
+              LanguagesUtils.getString(
+                  "How_make_better", "How can we make it better?"),
+              style:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           E.sizeBoxHeight_16,
           InkWellButton(
-            onTap: () => OpenUrlUtils.openUrlWithExternalApplication(linkContract),
+            onTap: () =>
+                OpenUrlUtils.openUrlWithExternalApplication(linkContract),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 12, bottom: 6),
-                  child: Text(LanguagesUtils.getString("please_contact_us", "Please contact us"),
+                  child: Text(
+                      LanguagesUtils.getString(
+                          "please_contact_us", "Please contact us"),
                       style: const TextStyle(fontSize: 13)),
                 ),
                 Text(linkContract,
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16, color: Colors.lightBlue, decoration: TextDecoration.underline)),
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.lightBlue,
+                        decoration: TextDecoration.underline)),
               ],
             ),
           ),
           E.sizeBoxHeight_32,
           ElevatedButton.icon(
             onPressed: () => RouteUtils.backPress(context: context),
-            label: Text(LanguagesUtils.getString("Close", "Close").toUpperCase()),
+            label:
+                Text(LanguagesUtils.getString("Close", "Close").toUpperCase()),
             icon: Icon(Icons.close_rounded),
           )
         ],
@@ -175,67 +190,69 @@ class _DialogRatingState extends State<DialogRating> {
   }
 
   Widget _buildRateStateNone(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset("packages/q_rate_dialog/imgs/ic_rate_dialog.png", width: 64, height: 64),
-          E.sizeBoxHeight_16,
-          Text(LanguagesUtils.getString("rate_msg1", "How was your experience with us?"),
-              style: T.textTheme(context).headlineSmall, textAlign: TextAlign.center),
-          E.sizeBoxHeight_16,
-          Center(
-            child: RatingBar.builder(
-              initialRating: 0,
-              minRating: 1,
-              direction: Axis.horizontal,
-              allowHalfRating: false,
-              itemCount: 5,
-              itemSize: 43,
-              itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
-              itemBuilder: (context, _) => Icon(
-                Icons.star,
-                color: Colors.amber,
-              ),
-              onRatingUpdate: (rating) {
-                setState(() {
-                  ratingValue = rating;
-                });
-              },
-            ),
-          ),
-          Text("${ratingValue.toInt()}/5",
-              style: T
-                  .textTheme(context)
-                  .headlineSmall
-                  ?.copyWith(color: T.getColors(context).primary, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center),
-          E.sizeBoxHeight_32,
-          Wrap(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset("packages/q_rate_dialog/imgs/ic_rate_dialog.png",
+            width: 64, height: 64),
+        E.sizeBoxHeight_16,
+        Text(
+            LanguagesUtils.getString(
+                "rate_msg1", "How was your experience with us?"),
+            style: T.textTheme(context).headlineSmall,
+            textAlign: TextAlign.center),
+        E.sizeBoxHeight_16,
+        Center(
+          child: RatingBar.builder(
+            initialRating: 0,
+            minRating: 1,
             direction: Axis.horizontal,
-            alignment: WrapAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: btnNotNowClick,
-                child: Text(
-                  LanguagesUtils.getString("Notnow", "Not now").toUpperCase(),
-                  style: TextStyle(fontSize: 18, color: Colors.black45),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
+            allowHalfRating: false,
+            itemCount: 5,
+            itemSize: 43,
+            itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+            itemBuilder: (context, _) => Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            onRatingUpdate: (rating) {
+              setState(() {
+                ratingValue = rating;
+              });
+            },
+          ),
+        ),
+        Text("${ratingValue.toInt()}/5",
+            style: T.textTheme(context).headlineSmall?.copyWith(
+                color: T.getColors(context).primary,
+                fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center),
+        E.sizeBoxHeight_32,
+        Wrap(
+          direction: Axis.horizontal,
+          alignment: WrapAlignment.spaceAround,
+          children: [
+            TextButton(
+              onPressed: btnNotNowClick,
+              child: Text(
+                LanguagesUtils.getString("Notnow", "Not now").toUpperCase(),
+                style: TextStyle(fontSize: 18, color: T.getColors(context).secondary),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
-              TextButton(
-                onPressed: btnSubmitClick,
-                child: Text(
-                  LanguagesUtils.getString("Submit", "Submit").toUpperCase(),
-                  style: TextStyle(fontSize: 18, color: T.getColors(context).primary),
-                ),
+            ),
+            TextButton(
+              onPressed: btnSubmitClick,
+              child: Text(
+                LanguagesUtils.getString("Submit", "Submit").toUpperCase(),
+                style: TextStyle(
+                    fontSize: 18, color: T.getColors(context).primary),
               ),
-            ],
-          )
-        ],
-      ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
